20e5948f19a313a44131ea880ec12e72346adc20
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2021-04-11 17:32:16 +0100

    Bump dependencies

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -1,14 +1,14 @@
 object PluginVersion {
     const val SPRING_BOOT = AppVersion.SPRING_BOOT
     const val SPRING_DEPENDENCY_MANAGEMENT = "1.0.11.RELEASE"
-    const val KOTLIN = "1.4.31"
+    const val KOTLIN = "1.4.32"
     const val KTLINT = "10.0.0"
 }
 
 object AppVersion {
-    const val SPRING_BOOT = "2.4.3"
-    const val COROUTINES = "1.4.2"
-    const val SPRING_DOC_OPEN_API = "1.5.4"
+    const val SPRING_BOOT = "2.4.4"
+    const val COROUTINES = "1.4.3"
+    const val SPRING_DOC_OPEN_API = "1.5.7"
 
     // Test
     const val MOCKK = "1.10.6"
